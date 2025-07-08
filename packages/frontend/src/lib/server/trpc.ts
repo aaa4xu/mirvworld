836ed1550c2d06709d3fbff3d8b36a93f3db3903
aaa4xu <aaa4xu@gmail.com>
2025-07-09 03:04:58 +0700

    🚑️ frontend: fix build of docker image

--- packages/frontend/src/lib/server/trpc.ts
+++ packages/frontend/src/lib/server/trpc.ts
@@ -3,5 +3,5 @@ import type { AppRouter } from 'backend';
 import { env } from '$env/dynamic/private';
 
 export const trpc = createTRPCProxyClient<AppRouter>({
-  links: [httpBatchLink({ url: env.BACKEND_ENDPOINT })],
+  links: [httpBatchLink({ url: env.BACKEND_ENDPOINT ?? 'http://localhost:4000' })],
 });
