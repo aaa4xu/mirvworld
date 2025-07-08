5385d4e309fb793e728e5b3931c1bcb7f070487d
aaa4xu <aaa4xu@gmail.com>
2025-07-08 14:57:42 +0700

    🔧 frontend: switch Discord OAuth to use dynamic environment variables

--- packages/frontend/src/lib/server/oauth.ts
+++ packages/frontend/src/lib/server/oauth.ts
@@ -1,5 +1,9 @@
 import { Discord } from 'arctic';
-import { OAUTH_DISCORD_CLIENT_ID, OAUTH_DISCORD_CLIENT_SECRET, OAUTH_DISCORD_REDIRECT_URI } from '$env/static/private';
+import { env } from '$env/dynamic/private';
 
 // TODO: Update redirect URI
-export const discord = new Discord(OAUTH_DISCORD_CLIENT_ID, OAUTH_DISCORD_CLIENT_SECRET, OAUTH_DISCORD_REDIRECT_URI);
+export const discord = new Discord(
+  env.OAUTH_DISCORD_CLIENT_ID,
+  env.OAUTH_DISCORD_CLIENT_SECRET,
+  env.OAUTH_DISCORD_REDIRECT_URI,
+);
