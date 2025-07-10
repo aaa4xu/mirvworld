844acf447d39447b57152435f7fb91521e7cd99c
aaa4xu <aaa4xu@gmail.com>
2025-07-11 03:04:21 +0700

    ✨ lurker: Add `startedAt` field to DownloadQueue and update pop logic

--- packages/lurker/src/DownloadQueue.ts
+++ packages/lurker/src/DownloadQueue.ts
@@ -10,6 +10,7 @@ export class DownloadQueue {
   public push(id: string, startedAt: number) {
     this.state.push({
       id,
+      startedAt,
       lastAttemptAt: startedAt,
       attempts: 0,
     });
@@ -17,13 +18,20 @@ export class DownloadQueue {
   }
 
   public pop() {
+    const now = Date.now();
     const entry = this.state
-      .filter((e) => e.lastAttemptAt + 60 * 1_000 < Date.now())
+      .filter((e) => {
+        if (e.startedAt && e.startedAt + 10 * 60 * 1000 > now) {
+          return false;
+        }
+
+        return e.lastAttemptAt + 60 * 1_000 < now;
+      })
       .sort((a, b) => a.lastAttemptAt - b.lastAttemptAt)[0];
 
     if (entry) {
       entry.attempts++;
-      entry.lastAttemptAt = Date.now();
+      entry.lastAttemptAt = now;
       return entry.id;
     } else {
       return null;
@@ -42,6 +50,7 @@ export class DownloadQueue {
 
 interface DownloadQueueEntry {
   id: string;
+  startedAt?: number;
   lastAttemptAt: number;
   attempts: number;
 }
