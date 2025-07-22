c529c6b1a68e169e4be4104b5bd253bca333d8bf
aaa4xu <aaa4xu@gmail.com>
2025-07-22 19:44:22 +0700

    🚑️ matches: Remove top-level async

--- packages/matches/index.ts
+++ packages/matches/index.ts
@@ -17,25 +17,25 @@ process.on('SIGTERM', () => abort.abort('SIGTERM'));
 process.on('SIGINT', () => abort.abort('SIGINT'));
 
 const db = drizzle(config.db);
-await migrate(db, { migrationsFolder: './drizzle' });
+migrate(db, { migrationsFolder: './drizzle' }).then(() => {
+  const redis = new RedisClient(config.redis);
+  const replaysS3 = new Client(config.replays.s3.endpoint);
+  const replayStorage = new ReplayStorage(new MinioStorage(config.replays.s3.bucket, replaysS3));
+  const eventsStorage = new GamelensEventsStorage(
+    new MinioStorage(config.gamelens.s3.bucket, new Client(config.gamelens.s3.endpoint)),
+  );
 
-const redis = new RedisClient(config.redis);
-const replaysS3 = new Client(config.replays.s3.endpoint);
-const replayStorage = new ReplayStorage(new MinioStorage(config.replays.s3.bucket, replaysS3));
-const eventsStorage = new GamelensEventsStorage(
-  new MinioStorage(config.gamelens.s3.bucket, new Client(config.gamelens.s3.endpoint)),
-);
+  if (!config.readOnly) {
+    const streamify = new Streamify(redis, 'storage:bucketevents', 'matches:processing');
+    abort.signal.addEventListener('abort', () => streamify.dispose());
+    streamify.start();
+    new MatchInfoImporter(redis, db, replayStorage);
+  }
 
-if (!config.readOnly) {
-  const streamify = new Streamify(redis, 'storage:bucketevents', 'matches:processing');
-  abort.signal.addEventListener('abort', () => streamify.dispose());
-  streamify.start();
-  new MatchInfoImporter(redis, db, replayStorage);
-}
+  const server = createHTTPServer({
+    router: appRouter,
+    createContext: createContext(db, eventsStorage),
+  });
 
-const server = createHTTPServer({
-  router: appRouter,
-  createContext: createContext(db, eventsStorage),
+  server.listen(config.http.port);
 });
-
-server.listen(config.http.port);
