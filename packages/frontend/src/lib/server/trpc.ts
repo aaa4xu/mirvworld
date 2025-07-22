081e7b804b7efaca8dd92cff6d7ac9af045b1fbf
aaa4xu <aaa4xu@gmail.com>
2025-07-22 17:38:04 +0700

    ✨ frontend: Use dynamic BACKEND_ENDPOINT and update Dockerfile accordingly

--- packages/frontend/src/lib/server/trpc.ts
+++ packages/frontend/src/lib/server/trpc.ts
@@ -1,11 +1,12 @@
 import { createTRPCClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from 'matches/src/trpc/router';
 import superjson from 'superjson';
+import { env } from '$env/dynamic/private';
 
 export const trpc = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: 'http://localhost:3600',
+      url: env.BACKEND_ENDPOINT ?? 'http://localhost:3600',
       transformer: superjson,
     }),
   ],
