019d8a1ca382e1de97342846d74e23d789ad68fb
Aleksey Orekhovsky <aaa4xu@gmail.com>
2025-08-05 20:34:10 +0700

    🐛 frontend: Add milliseconds tooltip to Duration component for enhanced readability

--- packages/frontend/src/lib/components/Duration.svelte
+++ packages/frontend/src/lib/components/Duration.svelte
@@ -4,8 +4,11 @@
   const durationH = Math.floor(seconds / 3600 / 1000);
   const durationM = Math.floor((seconds / 60 / 1000) % 60);
   const durationS = Math.floor((seconds / 1000) % 60);
+  const durationMS = ((seconds / 1000) % 60).toFixed(1);
 
   const toString = (value: number) => value.toString().padStart(2, '0');
 </script>
 
-{#if durationH > 0}{durationH}:{/if}{toString(durationM)}:{toString(durationS)}
+<span title="{durationH > 0 ? durationH + ':' : ''}{toString(durationM)}:{durationMS}"
+  >{#if durationH > 0}{durationH}:{/if}{toString(durationM)}:{toString(durationS)}</span
+>
