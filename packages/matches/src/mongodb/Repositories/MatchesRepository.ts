e7083fd38500d46508cc4fe8706b989223755fac
Aleksey Orekhovsky <aaa4xu@gmail.com>
2025-08-06 09:23:26 +0700

    ✨ matches: Improve DTO parsing with validation and error handling

--- packages/matches/src/mongodb/Repositories/MatchesRepository.ts
+++ packages/matches/src/mongodb/Repositories/MatchesRepository.ts
@@ -1,6 +1,7 @@
 import { Collection, type Db, ObjectId } from 'mongodb';
 import { type Match, type MatchDTO, MatchDTOSchema, type MatchInsert, MatchInsertSchema } from '../Models/Match.ts';
 import type { PlayerStats } from '@mirvworld/gamelens-stats';
+import z from 'zod/v4';
 
 export class MatchesRepository {
   private readonly collection: Collection<Omit<Match, '_id'>>;
@@ -25,27 +26,25 @@ export class MatchesRepository {
       .limit(20)
       .toArray();
 
-    return items.map((m) => this.toDTO(m));
+    return this.toArrayDTO(items);
   }
 
   public async read(id: MatchDTO['id']): Promise<MatchDTO | null> {
-    const item = await this.collection.findOne({ _id: new ObjectId(id) });
-    return item ? this.toDTO(item) : null;
+    return this.toDTO(await this.collection.findOne({ _id: new ObjectId(id) }));
   }
 
   public async readByGameId(gameId: string): Promise<MatchDTO | null> {
-    const item = await this.collection.findOne({ gameId: gameId });
-    return item ? this.toDTO(item) : null;
+    return this.toDTO(await this.collection.findOne({ gameId: gameId }));
   }
 
-  public async latest() {
+  public async latest(): Promise<Array<MatchDTO>> {
     /* The first 4 bytes of ObjectId is timestamp, so sorting by _id should by close enough to sorting by createdAt */
     const items = await this.collection
       .aggregate<Match>([{ $sort: { _id: -1 } }])
       .limit(20)
       .toArray();
 
-    return items.map((m) => this.toDTO(m));
+    return this.toArrayDTO(items);
   }
 
   public async setPlayers(id: MatchDTO['id'], players: Array<PlayerStats>) {
@@ -66,12 +65,25 @@ export class MatchesRepository {
     await this.collection.insertOne(parsed);
   }
 
-  private toDTO(match: Match): MatchDTO {
+  private toDTO(match: Match | null): MatchDTO | null {
+    if (!match) return null;
+
     const dto = {
       ...match,
       id: match._id.toString(),
     };
 
-    return MatchDTOSchema.parse(dto);
+    const result = MatchDTOSchema.safeParse(dto);
+
+    if (result.success) {
+      return result.data;
+    } else {
+      console.error(`[${this.constructor.name}][${match._id}] Failed to parse match:`, z.prettifyError(result.error));
+      return null;
+    }
+  }
+
+  private toArrayDTO(matches: Array<Match>): Array<MatchDTO> {
+    return matches.map((m) => this.toDTO(m)).filter((m): m is MatchDTO => m !== null);
   }
 }
